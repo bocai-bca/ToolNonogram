4954036d6872069a1f2da3307d68a096dff74fbe
bocai-bca <105545253+bocai-bca@users.noreply.github.com>
2025-03-13 12:25:36 +0800

    v5

--- contents/main/main.gd
+++ contents/main/main.gd
@@ -2,14 +2,18 @@ extends Node2D
 class_name Main
 ## 主场景根类
 
-## 伪单例
+## 伪单例FakeSingleton
 static var fs: Main
 
 @onready var n_back_color: ColorRect = $BackColor as ColorRect
 @onready var n_game_grids: GameGrids = $GameGrids as GameGrids
 
+
 func _enter_tree() -> void:
 	fs = self
 
 func _ready() -> void:
+	pass
+	
+func _process(delta: float) -> void:
 	n_back_color.size = get_window().size
--- contents/main/side_bar.gd
+++ contents/main/side_bar.gd
@@ -3,6 +3,23 @@ class_name SideBar
 ## 侧边栏。
 
 @onready var n_bar_color: ColorRect = $BarColor as ColorRect
+@onready var n_shadow: Sprite2D = $Shadow as Sprite2D
+@onready var n_buttons: Array[SideButton] = [
+	$SideButton_Misc as SideButton
+]
+
+## 侧边栏宽度比例，基于视口纵向长度
+const BAR_WIDTH_SCALE: float = 1.0 / 6.0
+## 阴影缩放X基值比例，基于视口纵向长度
+const SHADOW_SCALE_X_BASE_SCALE: float = 2.0 / 1080.0
+
+static var bar_width: float = 180.0
 
 func _process(delta: float) -> void:
-	pass
+	var window_size: Vector2 = Vector2(get_window().size)
+	position = Vector2(window_size.x, window_size.y / 2.0)
+	n_bar_color.size = Vector2(window_size.y * BAR_WIDTH_SCALE, window_size.y)
+	n_bar_color.position = Vector2(-n_bar_color.size.x, n_bar_color.size.y / -2.0)
+	n_shadow.scale = Vector2(SHADOW_SCALE_X_BASE_SCALE * window_size.y, window_size.y)
+	n_shadow.position = Vector2(n_bar_color.position.x - n_shadow.texture.get_size().x * n_shadow.scale.x / 2.0, 0.0)
+	## 接下来写侧边按钮的排列和缩放
