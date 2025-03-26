e98c23065cb9c61a035c1303f31b6d15923aee99
bocai-bca <105545253+bocai-bca@users.noreply.github.com>
2025-03-27 00:32:43 +0800

    v12

--- contents/main/number_bar/number_bar.gd
+++ contents/main/number_bar/number_bar.gd
@@ -7,7 +7,7 @@ static var fs: NumberBar
 
 @onready var n_frame_background_up: ColorRect = $FrameBackground_Up as ColorRect
 @onready var n_frame_background_side: ColorRect = $FrameBackground_Side as ColorRect
-@onready var n_icon_background: ColorRect = $IconBackground as ColorRect
+@onready var n_icon_fill_background: ColorRect = $IconFillBackground as ColorRect
 @onready var n_icon: Sprite2D = $Icon as Sprite2D
 
 ## 边框宽度率，影响边框厚度的乘数，基数为工具图标的边长
@@ -16,8 +16,8 @@ const FRAME_THICKNESS_RATE: float = 0.025
 # 	1.基于图标大小，这样边框相对于图标是固定粗细的，但是如果图标缩小，那么边框会一起缩得很细
 #	2.基于窗口大小，这样边框相对于窗口是固定粗细的，如果窗口分辨率太大的话，边框会一起变得很粗
 
-## 工具图标大小，单位是一条边的长度。(涵盖边框)
-static var icon_size: float = 270.0 #该值目前是硬编码数字，但它应该能改为其占据窗口的比率，正如侧边栏和图层栏一样
+## 工具图标大小，单位是一条边的长度(涵盖边框)。
+static var icon_size: float = 0.25 * Main.WINDOW_SIZE_DEFAULT.y
 
 func _enter_tree() -> void:
 	fs = self #定义伪单例
@@ -26,7 +26,7 @@ func _process(delta: float) -> void:
 	position.x = LayersBar.bar_width #更新数字栏坐标使其贴靠到图层栏的右上角
 	## 00更新工具图标和图标背景
 	var frame_thickness: float = icon_size * FRAME_THICKNESS_RATE #通过工具图标背景的边长和边框厚度乘数求
-	n_icon_background.size = Vector2(icon_size - 2.0 * frame_thickness, icon_size - 2.0 * frame_thickness) #计算工具图标背景大小并应用
-	n_icon_background.position = Vector2(frame_thickness, frame_thickness) #根据工具图标背景的大小计算坐标并引用
+	n_icon_fill_background.size = Vector2(icon_size - 2.0 * frame_thickness, icon_size - 2.0 * frame_thickness) #计算工具图标背景大小并应用
+	n_icon_fill_background.position = Vector2(frame_thickness, frame_thickness) #根据工具图标背景的大小计算坐标并引用
 	# 此处接着写工具图标的变换
 	## /00
