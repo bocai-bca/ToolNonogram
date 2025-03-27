f05ec4d7527793d23ab55bdedac931c4583959ee
bocai-bca <105545253+bocai-bca@users.noreply.github.com>
2025-03-27 12:05:55 +0800

    v13

--- contents/editable_grids/editable_grids.gd
+++ contents/editable_grids/editable_grids.gd
@@ -1,5 +1,27 @@
 extends Node2D
 class_name EditableGrids
-## 游戏网格。用于显示和容纳版面网格和棋子的类
+## 游戏版面可编辑网格。用于显示和容纳版面网格和棋子的类
 
 @onready var n_grid_map: TileMapLayer = $GridMap as TileMapLayer
+
+## 砖瓦一般大小(边长)，单位是像素，该值需要手动参考tile_set中的砖瓦像素尺寸设定值。用于参与网格的变换计算
+const TILE_NORMAL_SIZE: float = 16.0
+
+## 显示偏移量(坐标移动偏移量)，表示当前画面中最左上角的格子应当是网格中的哪个坐标的格子
+static var display_offset: Vector2i = Vector2i(0, 0)
+## 网格尺寸
+static var grid_size: Vector2i = Vector2i(5, 5) #网格实例的节点的初始尺寸是5*5
+
+func _process(delta: float) -> void:
+	var window_size: Vector2 = Vector2(get_window().size) #获取窗口大小
+	## 00更新网格的变换
+	var free_height: float = window_size.y - NumberBar.icon_size #计算可用空间高度
+	scale.y = free_height / (Main.grids_zoom_blocks * TILE_NORMAL_SIZE) #计算缩放的值
+	scale.x = scale.y #将缩放向量的Y复制到X
+	var actually_offset: Vector2 = display_offset * TILE_NORMAL_SIZE #计算实际的坐标移动偏移量
+	position = Vector2(LayersBar.bar_width + NumberBar.icon_size, NumberBar.icon_size) - actually_offset #计算坐标并应用
+	## /00
+
+## 重新设置网格尺寸(未来会写显示不同内容的砖瓦图节点，那些节点不受本函数影响，本函数应当只负责改变背景的大小
+func resize_grid(new_size: Vector2i) -> void:
+	pass
--- contents/main/number_bar/number_bar.gd
+++ contents/main/number_bar/number_bar.gd
@@ -2,11 +2,18 @@ extends Node2D
 class_name NumberBar
 ## 数字栏。涵盖题目数字横行、竖列和工具图标的节点树枝
 
+## 目前有一个争议，就是图标的缩放方法、边框的缩放方法。边框目前打算跟随网格的缩放，使得全局的边框统一。图标打算也像网格一样保存一个变量记录缩放率，然后跟随窗口的变化一起变化
+## 对于接下来的修改，只需要改一下边框厚度乘数这个量的取值方式即可，其他计算全都依靠该变量
+
+## 边框的厚度接下来可能完全取决于砖瓦的边框，请思考如何控制砖瓦边框的厚度
+
 ## 伪单例FakeSingleton
 static var fs: NumberBar
 
 @onready var n_frame_background_up: ColorRect = $FrameBackground_Up as ColorRect
 @onready var n_frame_background_side: ColorRect = $FrameBackground_Side as ColorRect
+@onready var n_fill_background_up: ColorRect = $FillBackground_Up as ColorRect
+@onready var n_fill_background_side: ColorRect = $FillBackground_Side as ColorRect
 @onready var n_icon_fill_background: ColorRect = $IconFillBackground as ColorRect
 @onready var n_icon: Sprite2D = $Icon as Sprite2D
 
@@ -15,18 +22,37 @@ const FRAME_THICKNESS_RATE: float = 0.025
 # 此乘数应用于什么基数是一个有待选择的问题，有两种选择：(目前选择1)
 # 	1.基于图标大小，这样边框相对于图标是固定粗细的，但是如果图标缩小，那么边框会一起缩得很细
 #	2.基于窗口大小，这样边框相对于窗口是固定粗细的，如果窗口分辨率太大的话，边框会一起变得很粗
+## 缩放率调整步长，使得调整一次缩放率能像调整一次网格一样具有固定的步长
+const ZOOM_RATE_STEP_LENGTH: float = 0.1
+## 默认缩放率
+const ZOOM_RATE_DEFAULT: float = 0.2
+## 缩放率边界值，用于限制避免缩放率超出合适范围，X表示最小值、Y表示最大值
+const ZOOM_RATE_BOUND: Vector2 = Vector2(0.1, 0.4)
 
 ## 工具图标大小，单位是一条边的长度(涵盖边框)。
-static var icon_size: float = 0.25 * Main.WINDOW_SIZE_DEFAULT.y
+static var icon_size: float = Main.WINDOW_SIZE_DEFAULT.y * ZOOM_RATE_DEFAULT
+## 缩放率，表示工具图标占据窗口纵向长度的比率
+static var zoom_rate: float = ZOOM_RATE_DEFAULT
 
 func _enter_tree() -> void:
 	fs = self #定义伪单例
 
 func _process(delta: float) -> void:
+	var window_size: Vector2 = Vector2(get_window().size) #获取窗口尺寸
 	position.x = LayersBar.bar_width #更新数字栏坐标使其贴靠到图层栏的右上角
-	## 00更新工具图标和图标背景
-	var frame_thickness: float = icon_size * FRAME_THICKNESS_RATE #通过工具图标背景的边长和边框厚度乘数求
+	## 00更新图标大小变量
+	icon_size = window_size.y * zoom_rate #计算新的图标大小
+	## /00
+	## 01更新工具图标和图标背景
+	var frame_thickness: float = icon_size * FRAME_THICKNESS_RATE #通过工具图标背景的边长和边框厚度乘数求一条边框的实际厚度
 	n_icon_fill_background.size = Vector2(icon_size - 2.0 * frame_thickness, icon_size - 2.0 * frame_thickness) #计算工具图标背景大小并应用
 	n_icon_fill_background.position = Vector2(frame_thickness, frame_thickness) #根据工具图标背景的大小计算坐标并引用
 	# 此处接着写工具图标的变换
-	## /00
+	## /01
+	## 02更新边框背景和填充背景
+	n_frame_background_up.size = Vector2(window_size.x - position.x, icon_size) #更新顶部边框背景的尺寸
+	n_frame_background_side.size = Vector2(icon_size, window_size.y) #更新侧边边框背景的尺寸
+	n_fill_background_up.position = Vector2(window_size.y + frame_thickness, frame_thickness) #更新顶部填充背景的坐标
+	n_fill_background_up.size = Vector2(clampf(window_size.x - frame_thickness - window_size.y - LayersBar.bar_width, 0.0, window_size.x), icon_size - 2.0 * frame_thickness) #更新顶部填充背景的尺寸
+	#n_fill_background_side.position = Vector2() #更新侧边填充背景的坐标
+	## /02
